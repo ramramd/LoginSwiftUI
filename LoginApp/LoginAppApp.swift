5129324b431aa89998da243cd068a8d1bf988a01
Naveen Ramachandrappa <naveen_ramachandrappa@apple.com>
2023-05-08 12:32:14 -0500

    Initial Commit

---
+++ LoginApp/LoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppApp.swift
+//  LoginApp
+//
+//  Created by Naveen Ramachandrappa on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
